adb3675945f42fb300e86f961f28f13c3bdd4770
Dio Lemos <pedrodiogenes@outlook.com>
2022-01-28 13:53:02 +0000

    Update script.js

--- script.js
+++ script.js
@@ -3,18 +3,25 @@
 
 let i = 1;
 let playersNum;
-playersNum = document.getElementById("input").value;
+
 let answerBoardNode = document.getElementById("answerBoard");
 let answer = []
 
 function fizzBuzz() {
+    playersNum = document.getElementById("input").value;
     while (i <= playersNum) {
         if (playersNum % 3 == 0 && playersNum % 5 === 0) {
-            answerBoardNode.innerHTML = "FizzBuzz"
+            console.log("FizzBuzz")
+        } else if (playersNum % 3 == 0) {
+            console.log(Fizz)
+        } else if (playersNum % 5 == 0) {
+            console.log("Buzz")
+        } else {
+            console.log(i)
         }
 
         i++
     }
-
+    fizzBuzz()
 
 }
